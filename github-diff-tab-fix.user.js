6fd3d76d6b582d044e3e1e842be1239fac3762c6
Brad Jorsch <anomie@users.sourceforge.net>
2022-07-29 09:42:45 -0400

    github-diff-tab-fix: Watch for style removal
    GitHub removes our added `<style>` when switching tabs. Sigh. Watch for
    that and re-add it as necessary.
    
    Also, put the styles into the tag as a text node rather than using
    `style.sheet.insertRule()`. Rules inserted with the latter method are
    lost if the tag gets removed from the DOM.

--- github-diff-tab-fix.user.js
+++ github-diff-tab-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GitHub Diff Tab Fix
 // @namespace   https://github.com/anomiex/greasemonkey-scripts
-// @version     1.1
+// @version     1.2
 // @description Fixes tab stops in GitHub's diff display in Firefox.
 // @grant       none
 // @match       https://github.com/*
@@ -9,21 +9,33 @@
 
 // You can test alignment with https://github.com/anomiex/testing/commit/3959bacdf3f59aeaaf87ff1a3bf0ca8d630c1f5a
 
-const head = document.getElementsByTagName( 'head' )[ 0 ];
 const style = document.createElement( 'style' );
-
 style.setAttribute( 'type', 'text/css' );
-head.appendChild( style );
 
-function addRule( rule ) {
-	style.sheet.insertRule( rule, style.sheet.rules.length );
+// Do it this way instead of with style.sheet.insertRule because the latter is lost when the tag is removed from the DOM and re-added, while this is kept.
+style.appendChild( document.createTextNode( `
+/* Firefox applies tab-stop based on the nearest block container's fonts. Make the span a block container. */
+span.blob-code-inner {
+	display: block;
 }
 
-// Firefox applies tab-stop based on the nearest block container's fonts. Make the span a block container.
-addRule( 'span.blob-code-inner { display: block; }' );
+/* No clue why GitHub sets text-indent to -7px here. */
+.soft-wrap .blob-code {
+	text-indent: 0;
+}
 
-// No clue why GitHub sets text-indent to -7px here.
-addRule( '.soft-wrap .blob-code { text-indent: 0; }' );
+/* For some unknown reason this has different padding-left from \`.soft-wrap .blob-code-context\`. */
+.diff-table .blob-code.blob-code-inner {
+	padding-left: 24px;
+}
+` ) );
 
-// For some unknown reason this has different padding-left from `.soft-wrap .blob-code-context`.
-addRule( '.diff-table .blob-code.blob-code-inner { padding-left: 24px; }' );
+document.head.appendChild( style );
+
+// GitHub will sometimes remove our stylesheet from the DOM. Sigh. Watch for that and re-add it.
+const observer = new MutationObserver( () => {
+	if ( ! style.parentNode ) {
+		document.head.appendChild( style );
+	}
+} );
+observer.observe( document.head, { childList: true } );
