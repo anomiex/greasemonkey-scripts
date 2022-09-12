ae3f68c167246b082a2e0565383209992a35b5b2
Brad Jorsch <anomie@users.sourceforge.net>
2022-09-12 08:45:54 -0400

    GitHub fixed the diff tabs, stub out the script
    If I just delete it Greasemonkey will probably keep it around in
    peoples' browsers forever, while this way maybe they'll see it to clean
    up their installation.

--- github-diff-tab-fix.user.js
+++ github-diff-tab-fix.user.js
@@ -1,41 +1,12 @@
 // ==UserScript==
-// @name        GitHub Diff Tab Fix
+// @name        GitHub Diff Tab Fix (obsolete)
 // @namespace   https://github.com/anomiex/greasemonkey-scripts
-// @version     1.2
-// @description Fixes tab stops in GitHub's diff display in Firefox.
+// @version     1.3
+// @description Script formerly working around a now-fixed bug
 // @grant       none
 // @match       https://github.com/*
 // ==/UserScript==
 
+// The bug this script worked around has now been fixed. You should probably uninstall it.
+
 // You can test alignment with https://github.com/anomiex/testing/commit/3959bacdf3f59aeaaf87ff1a3bf0ca8d630c1f5a
-
-const style = document.createElement( 'style' );
-style.setAttribute( 'type', 'text/css' );
-
-// Do it this way instead of with style.sheet.insertRule because the latter is lost when the tag is removed from the DOM and re-added, while this is kept.
-style.appendChild( document.createTextNode( `
-/* Firefox applies tab-stop based on the nearest block container's fonts. Make the span a block container. */
-span.blob-code-inner {
-	display: block;
-}
-
-/* No clue why GitHub sets text-indent to -7px here. */
-.soft-wrap .blob-code {
-	text-indent: 0;
-}
-
-/* For some unknown reason this has different padding-left from \`.soft-wrap .blob-code-context\`. */
-.diff-table .blob-code.blob-code-inner {
-	padding-left: 24px;
-}
-` ) );
-
-document.head.appendChild( style );
-
-// GitHub will sometimes remove our stylesheet from the DOM. Sigh. Watch for that and re-add it.
-const observer = new MutationObserver( () => {
-	if ( ! style.parentNode ) {
-		document.head.appendChild( style );
-	}
-} );
-observer.observe( document.head, { childList: true } );
